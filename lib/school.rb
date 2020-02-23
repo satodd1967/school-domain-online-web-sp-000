417fdcac5cabbee190d1905a8305cad1edc8cdea
Seth Todd <seth.todd@outlook.com>
2020-02-23 05:54:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,7 @@
 class School
-  attr_accessor :roster, :student_name :
-  def initialize(name)
+  attr_accessor :roster, :student_name :school_name
+  def initialize(school_name)
     @name = name
+    @roster = {}
   end
 end
