2d57bf30ee5acf31dde56e953fb7d1369827f6c1
Seth Todd <seth.todd@outlook.com>
2020-02-23 05:53:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,6 @@
 class School
-  attr_reader :roster
+  
   def initialize(name)
     @name = name
-  end
-  
-  def roster
-    @roster = {}
-  end
-  
-  def add_student(name, grade)
-    roster[grade] = []
-    roster[grade] << name
   end
 end
